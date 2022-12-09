bf76c7db4105374bce9f4e19e26bb7bdb6c1d1d3
KryaMaybe <83445675+KryaMaybe@users.noreply.github.com>
2022-12-10 00:12:36 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+#API Token
+API_TOKEN = "5719575885:AAFVoKrhl_J-y48HG6QzgNdNJwG67dxA9wk"
+
+#Admin ID
+admin_id = 910886732
+
+#Database connection settings
+dbHost = "sql.freedb.tech"
+dbUser = "freedb_KryaMaybe"
+dbPassword = "T6skcyH8%NrgH4Y"
+dbName = "freedb_KryaMaybe"
